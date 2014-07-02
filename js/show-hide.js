4c88086387f892f0ec5d9d009695c22a10d68d3c
tutsana <helzingg@gmail.com>
2014-07-02 23:03:33 +0300

    Revert "c-- article added"
    This reverts commit 915d650721b9afd767bd0f0fa74b7e698cddf738.

--- js/show-hide.js
+++
@@ -1,7 +0,0 @@
-function showHide(spoiler){
-
-if (document.getElementById('spoiler').style.display=='none') 
-	{document.getElementById('spoiler') .style.display=''}
-else
-	{document.getElementById('spoiler') .style.display='none'}
-}
