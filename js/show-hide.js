f26db9e18fbbfc214956915ad884025417f12703
D-N-D <dimitar.nikolaev.dimitrov@gmail.com>
2014-07-02 23:02:47 +0300

    c-- article added
    c--

---
+++ js/show-hide.js
@@ -0,0 +1,7 @@
+function showHide(spoiler){
+
+if (document.getElementById('spoiler').style.display=='none') 
+	{document.getElementById('spoiler') .style.display=''}
+else
+	{document.getElementById('spoiler') .style.display='none'}
+}
